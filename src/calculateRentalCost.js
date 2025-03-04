943b1e41c9c94ab05176b005b0199ea823a21ba1
Anori <anorifrell@gmail.com>
2025-03-04 21:30:34 +0200

    fixed changes\

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,7 +8,7 @@ function calculateRentalCost(days) {
     return days * 40;
   } else if (days <= 6) {
     return days * 40 - 20;
-  } else if (days >= 7) {
+  } else {
     return days * 40 - 50;
   }
 }
